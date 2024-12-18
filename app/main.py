b81dbc4f7e4962d4f13fe05efa7c658118abeb46
admin <aziat@astra>
2024-12-19 01:11:26 +0300

    Забыл уже какая версия

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@db:80/db}'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@db/db}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
