4d513f28d86a595a9477130085330d21e92b281b
admin <aziat@astra>
2024-12-19 05:02:08 +0300

    The End...

--- app/main.py
+++ app/main.py
@@ -1,35 +1,3 @@
-'''
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-import time
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@db/db'
-db = SQLAlchemy(app)
-
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-
-@app.route('/items', methods=['GET'])
-def get_items():
-    items = Item.query.all()
-    return jsonify([{'id': item.id, 'name': item.name} for item in items])
-
-@app.route('/items', methods=['POST'])
-def create_item():
-    data = request.json
-    new_item = Item(name=data['name'])
-    db.session.add(new_item)
-    db.session.commit()
-    return jsonify({'id': new_item.id, 'name': new_item.name}), 201
-
-if __name__ == '__main__':
-    time.sleep(6)
-    with app.app_context():
-        db.create_all()
-    app.run(host='0.0.0.0', port=80)
-'''
 from flask import Flask, request, jsonify
 import psycopg2
 import time
